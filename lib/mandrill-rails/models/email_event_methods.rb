2c57027c8dad806dfc5857fafa197975f98aa3d3
Jade Tremblay <jadetremblay.ing@gmail.com>
2014-07-21 21:30:00 -0400

    updated the model attribute to use concern

--- lib/mandrill-rails/models/email_event_methods.rb
+++ lib/mandrill-rails/models/email_event_methods.rb
@@ -3,16 +3,17 @@ require 'date'
 module Mandrill
   module Models
     module EmailEventMethods
-      def self.included(base)
+      extend ActiveSupport::Concern
+      included do
         # Associations
-        base.send :has_one, :msg
-        base.send :has_one, :user_agent_parsed
-        base.send :has_one, :location
+        has_one :msg
+        has_one :user_agent_parsed
+        has_one :location
         
         # Whitelisting attributes
-        base.send :attr_accessible, :event, :internal_id, :ip, :ts, :url, :user_agent, :msg_attributes, :user_agent_parsed_attributes, :location_attributes
+        attr_accessible :event, :internal_id, :ip, :ts, :url, :user_agent, :msg_attributes, :user_agent_parsed_attributes, :location_attributes
         
-        base.send :accepts_nested_attributes_for, :msg, :user_agent_parsed, :location
+        accepts_nested_attributes_for :msg, :user_agent_parsed, :location
         
       end
       
